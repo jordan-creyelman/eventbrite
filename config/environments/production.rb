a996290c7f8b90b75567e3b345592359b9a2b33b
jordan-creyelman <jordancreyelman1992@gmail.com>
2021-11-10 18:57:48 +0100

    first commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,17 +98,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # # host = "https://meetup-geneva.herokuapp.com/"
   # # config.action.mailer.default_url_options = {host: host}
-   config.action_mailer.default_url_options = { host: "https://peaceful-brushlands-18116.herokuapp.com/"}
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-  :address => 'smtp.gmail.com',
-  :port => 587,
-  :domain => 'gmail.com', #you can also use google.com
-  :authentication => :plain,
-  :user_name => "jordancreyelman1992@gmail.com",
-  :password => "azertyazerty18",
-  :enable_starttls_auto => true
-  }
+  #  config.action_mailer.default_url_options = { host: "https://peaceful-brushlands-18116.herokuapp.com/"}
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.smtp_settings = {
+  # :address => 'smtp.gmail.com',
+  # :port => 587,
+  # :domain => 'gmail.com', #you can also use google.com
+  # :authentication => :plain,
+  # :user_name => "jordancreyelman1992@gmail.com",
+  # :password => "azertyazerty18",
+  # :enable_starttls_auto => true
+  # }
 
 
   
