47282a40ce0ab9657fdc4bd6b324a90a4d7ea2ec
jordan-creyelman <jordancreyelman1992@gmail.com>
2021-11-10 14:02:45 +0100

    first commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,15 +99,15 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.smtp_settings = {
-  :address => 'smtp.gmail.com',
-  :port => 587,
-  :domain => 'gmail.com', #you can also use google.com
-  :authentication => :plain,
-  :user_name => "jordancreyelman1992@gmail.com",
-  :password => "azertyazerty18",
-  :enable_starttls_auto => true
-  }
+  # config.action_mailer.smtp_settings = {
+  # :address => 'smtp.gmail.com',
+  # :port => 587,
+  # :domain => 'gmail.com', #you can also use google.com
+  # :authentication => :plain,
+  # :user_name => "jordancreyelman1992@gmail.com",
+  # :password => "azertyazerty18",
+  # :enable_starttls_auto => true
+  # }
 
 
   
