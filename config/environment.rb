dd8db7a8ef0f2237bb0ef28bd847a47be43273d1
jordan-creyelman <jordancreyelman1992@gmail.com>
2021-11-10 15:58:13 +0100

    first commit

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :domain => 'gmail.com', #you can also use google.com
+  :authentication => :plain,
+  :user_name => "jordancreyelman1992@gmail.com",
+  :password => "azertyazerty18",
+  :enable_starttls_auto => true
+}
