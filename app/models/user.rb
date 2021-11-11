652f420a3abfd690eb98f996fa7d155ac6ba86ca
jordan-creyelman <jordancreyelman1992@gmail.com>
2021-11-11 11:43:09 +0100

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,26 @@
 class User < ApplicationRecord
 
-  after_create :welcome_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:trackable
-has_many :attendances
-has_many :events, through: :attendances
-has_many :admin_events, foreign_key: "event_admin_id", class_name: "Event"
-       
-def welcome_send
-  UserMailer.welcome_email(self).deliver_now
-end      
+         :recoverable, :rememberable, :validatable
+  after_create :welcome_send
+
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  has_many :administrated_events, foreign_key: 'admin_id', class_name: 'Event'
+
+  has_many :attendances
+  has_many :events, through: :attendances
+
+  private
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end     
 end
