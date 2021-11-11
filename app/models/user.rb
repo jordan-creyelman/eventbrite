4604377b6afc3f5dcc6c07bcc8941d2c1e2ee00b
jordan-creyelman <jordancreyelman1992@gmail.com>
2021-11-11 11:36:50 +0100

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,:trackable
 has_many :attendances
 has_many :events, through: :attendances
-has_many :admin_events, foreign_key: "event_admin_id", class_name: "Event", dependent: :destroy
+has_many :admin_events, foreign_key: "event_admin_id", class_name: "Event", 
        
 def welcome_send
   UserMailer.welcome_email(self).deliver_now
-      end      
+end      
 end
